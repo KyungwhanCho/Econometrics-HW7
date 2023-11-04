6ebd51e23187aab38ef243c20d4cfbee2d68efee
KyungwhanCho <144467386+KyungwhanCho@users.noreply.github.com>
2023-11-03 20:15:21 -0400

    Add files via upload

--- Lab 7 Female Public work sector.R
+++ Lab 7 Female Public work sector.R
@@ -21,19 +21,21 @@ levels_new_pub <- join(data.frame(levels_orig),data.frame(levels_public))
 levels(acs2021$IND) <- levels_new$New_Level
 levels(acs2021$public_work) <- levels_new_pub$New_Level
 
+## Transfer imported data set as numeric data and entitlement.
+
 acs2021$public_work_num <- as.numeric(acs2021$public_work == "work for public, stable")
 table(acs2021$public_work,acs2021$public_work_num)
 
-## The subgroup I chose, 22~55 years old female group who are fully employed
+## The subgroup I chose: 22~55 years old African American female group who are fully employed
 
 attach(acs2021)
-use_varb <- (AGE >= 25) & (AGE <= 55) & (LABFORCE == 2) & (WKSWORK2 > 4) & (UHRSWORK >= 35) & (female == 1)
-dat_use <- subset(acs2021,use_varb) # 
+use_varb <- (AGE >= 25) & (AGE <= 55) & (LABFORCE == 2) & (WKSWORK2 > 4) & (UHRSWORK >= 35) & (female == 1) & (AfAm == 1)
+dat_use <- subset(acs2021,use_varb) 
 detach(acs2021)
 
-## Null hypothesis: There is not a correlation between gender and proportion of people who are working in public work sector
+## Null hypothesis: There is not a correlation between being an African American female and proportion of people who are working in public work sector
 
-ols_out1 <- lm(public_work_num ~ female + educ_hs + educ_somecoll + educ_college + educ_advdeg + AGE, data = dat_use)
+ols_out1 <- lm(public_work_num ~ female + AfAm + educ_hs + educ_somecoll + educ_college + educ_advdeg + AGE, data = dat_use)
 summary(ols_out1, type = "text")
 
 ## p-value: < 2.2e-16 / Our p-value was very small, therefore, I figure there are enough evidence to reject the null hypothesis.
